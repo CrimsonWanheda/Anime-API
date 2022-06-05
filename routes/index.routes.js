8a8f039979a27ac199f53428c2e951e457553bb6
Jush <johnalghul@hotmail.com>
2022-06-05 04:32:49 -0500

    se corrigio error en git

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -4,7 +4,11 @@ import fs from 'fs';
 const router = Router();
 
 router.get('/', (req, res)=>{
+<<<<<<< HEAD
   res.render("index");
+=======
+  res.render("main")
+>>>>>>> 5585b073b9cd802f93f5e9f695996507261980ee
   
 })
 
